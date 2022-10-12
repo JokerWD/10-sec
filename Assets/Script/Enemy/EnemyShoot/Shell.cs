14e460bf1539174f03cc24915f97b9cf8a726b4d
JokerWD <joker.osnova1902@gmail.com>
2022-10-13 00:15:32 +0300

    ScriptableObject shell

--- Assets/Script/Enemy/EnemyShoot/Shell.cs
+++ Assets/Script/Enemy/EnemyShoot/Shell.cs
@@ -4,14 +4,26 @@ namespace TenEnemy
 {
     public abstract class Shell : MonoBehaviour
     {
+        [SerializeField] private ShellDate date;
         [SerializeField] private Rigidbody2D shellRigidbody2D;
-        [SerializeField] private float speed;
-        [SerializeField] protected int timeToDeath;
+        [SerializeField] private SpriteRenderer spriteRenderer;
+
         public Vector2 MoveDirection { private get; set; }
         
+        protected int timeToDeath;
+
+        private float _speed;
+
+        private void Awake()
+        {
+            _speed = date.Speed;
+            timeToDeath = date.TimeToDeath;
+            spriteRenderer.sprite = date.Sprite;
+        }
+
         protected void SetDirection()
         {
-            MoveDirection *= speed;
+            MoveDirection *= _speed;
             shellRigidbody2D.velocity = new Vector2(MoveDirection.x, MoveDirection.y);
         }
     }
